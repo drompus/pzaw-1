333445ba2ae4683b159b24efe775c050e9dadc5a
Dominik Mazurek <drompmaz2008@gmail.com>
2025-10-08 07:33:22 +0000

    added favicon and directory changes

--- projekt01/src/path_handlers.js
+++ projekt01/src/path_handlers.js
@@ -1,6 +1,7 @@
 import { readFileSync } from "node:fs";
 
 const index_html = readFileSync("static/index.html");
+const favicon_ico = readFileSync("static/img/favicon.ico");
 
 const pathConfigs = [
     {
@@ -12,13 +13,13 @@ const pathConfigs = [
         },
     },
     {
-        path: "/hello",
+        path: "/favicon.ico",
         allowed_methods: ["GET"],
         handler: (req, res) => {
-            res.writeHead(200, { "Content-Type": "text/plain" });
-            res.end("hello world!\n");
-        },
-    },
+            res.writeHead(200, { "Content-Type": "image/vnd.microsoft.icon" });
+            res.end(favicon_ico)
+        }
+    }
 ];
 
 export function handlePath(path, req, res) {
